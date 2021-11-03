2c5f1e1f38c4eaec73a0d5361a06538d3f2d6d0c
Jose Rodrigo Infante Torres <93353889+Otrick05@users.noreply.github.com>
2021-11-03 10:26:05 -0700

    ya esta listo

--- mCajero.cpp
+++ mCajero.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <cmath>
 #include <string.h>
 using namespace std;
 
 int main ()
 {
-string user1="102030", user2="302010", user3="704010", userR, admin="192784",usern,dinero1;
-int puser1=2010, puser2=1020, puser3=4010, puserR,answ, entrada,puserad=2719,balance,dinero;
-
+string user1="102030", user2="302010", user3="704010", userR, admin="192784",usern,tamnips;
+int passuser1=2010, passuser2=1020, passuser3=4010, puserR,answ, entrada=0,puserad=2719,balance,accbal1,accbal2,accbal3,dinero,newpass,tamnip;
+accbal1=(passuser1*10);
+accbal2=(passuser2*10);
+accbal3=(passuser3*10);
 do 
 {
 
@@ -17,28 +20,26 @@ do
      cin >> userR;
     cout << ("Ingrese su PIN: '\n'");
      cin >> puserR;
-if (userR==user1 and puserR==puser1) // entrada a la cuenta 1
+if (userR==user1 and puserR==passuser1) // entrada a la cuenta 1
 {
     entrada = 1;
-    
-    balance=(puser1*10);
+    balance=accbal1;
     usern="Pito perez";
     cout << ("Bienvenido(a) " + usern); 
     cout<<("'\n'");
 }
-    else if (userR==user2 and puserR==puser2) // entrada a la cuenta 2
+    else if (userR==user2 and puserR==passuser2) // entrada a la cuenta 2
     {
     entrada=2;
-    
-    balance=(puser2*10);
+    balance=accbal2; 
     usern="Juan Corrido";
     cout << ("Bienvenido(a) " + usern); 
     cout<<("'\n'");
     }
-    else if (userR==user3 and puserR==puser3){ // entrada a la cuenta 3
+    else if (userR==user3 and puserR==passuser3){ // entrada a la cuenta 3
     entrada=3;
     
-    balance=(puser3*10);
+    balance=accbal3; 
     usern="Maria Palomares";
     cout << ("Bienvenido(a) " + usern); 
     cout<<("'\n'");
@@ -55,28 +56,27 @@ if (userR==user1 and puserR==puser1) // entrada a la cuenta 1
 
 
  if (entrada==4)
-{
-    cout << "Welcome Dear Admin, the system will be close";
-}
+ {
+ cout << "Welcome Dear Admin, the system will be close";
+ }
 else if (entrada>0 and entrada <4)
 {
-do
-{
- cout << ("Desea : '\n'"); 
- cout << ("1.- Retirar'\n'"); 
- cout << ("2.- Consultar Saldo'\n'"); 
- cout << ("3.- Transferir a otra cuenta'\n'");
- cout << ("4.- Consultar Saldo'\n'");
- cout << ("5.- Cambiar NIP'\n'");   
- cout << ("0.- Salir'\n'"); 
- cin>> answ;
+ do
+ {
+  cout << ("Desea : '\n'"); 
+  cout << ("1.- Retirar'\n'"); 
+  cout << ("2.- Consultar Saldo'\n'"); 
+  cout << ("3.- Transferir a otra cuenta'\n'");
+  cout << ("4.-Cambiar NIP'\n'");  
+  cout << ("0.- Salir'\n'"); 
+  cin>> answ;
+
 switch (answ)
 {
 case 1:// caso para retiro
     
     cout << ("Cuanto desea retirar : '\n'"); 
-    cout << ("Su saldo es : $" );
-    cout << (balance);
+    cout << ("Su saldo es : $" ) << (balance);   
     cout << (".00 \n'");
     cout << ("1.-$100 '\n'");
     cout << ("2.-$200 '\n'");
@@ -85,34 +85,170 @@ case 1:// caso para retiro
     cout << ("Si desea otra cantidad digitela en multiplos de 100:'\n'");
     cout << ("Recuerde que solo puede retirar $9600: por transaccion: ");
     cin >> (answ);
+   if (answ ==4 and balance>=1000)
+    {
+    balance=balance-1000;
+     cout << ("Su saldo es :$")<< balance <<("\n");    
+    }
+    else if ( answ==3 and balance>=500)
+    {
+    balance=balance-500;
+     cout << ("Su saldo es :$")<< balance <<("\n");    
+    }
+    else if (answ==2 and balance>=200)  
+    {
+        balance=balance-200;
+         cout << ("Su saldo es :$")<< balance <<("\n");    
+    }
+    else if (answ==1 and balance>=100)
+    {
+        balance=balance-100;
+         cout << ("Su saldo es :$")<< balance <<("\n");    
+    }
+    else if (answ >=100 and answ <=9600 ) 
+    {      
+        if (answ > balance)
+        {
+            cout << ("Saldo insuficiente'\n'");
+        }
+        else if (balance>=answ)
+        {
+        dinero=(answ/100);  
+        balance=balance-(dinero*100);
+        cout << ("Su saldo es :$")<< balance <<("\n");
+        dinero=0;    
+        }       
+    }
+
    
-    dinero1=answ;
-    dinero=dinero1.length();
-
-    if  (dinero = 5)
+    else
     {
-        cout << ("Recuerde que solo puede sacar 9600 al mes");
+        cout<<("Digite una cantidad u opción correcta porfavor!'\n'" );
     }
-    else 
-    {
-
-    }
-
-
-
     break; //break case 1
+if (entrada==1)
+{
+accbal1=balance;
+
+}
+else if (entrada==2)
+{
+    accbal2=balance;
+    //balance=0;
+} 
+else if (entrada=3)
+{
+    accbal3=balance;
+    //balance=0;
+}
+
+
 case 2:// caso para consultar saldo
-    /* code */
+    cout << ("Su saldo es :$")<< balance <<("\n");   
     break;
 case 3:
-    /* code */
-    break;
-case 4:
-    /* code */
-    break;
-case 5:
-    /* code */
+    cout << ("Su saldo es :$")<< balance <<("\n");   
+    cout << ("Digite el número de cuenta '\n'");
+    cout << ("por lo pronto contamos con las siguientes opciones :'\n'");
+    cout << ("1.-") << passuser1 <<("'\n'");
+    cout << ("2.-") << passuser2 <<("'\n'");
+    cout << ("3.-") << passuser3 <<("'\n'");
+    cin>> answ;
+    cout << ("Digite la cantidad de la transferencia :$");
+    cin >> dinero;
+    cout <<("'\n'");
+    
+    if (answ==1 and dinero>=1)// if de transferencia
+    {
+    
+        if (balance>=dinero)
+        {
+         balance=balance-dinero;
+         cout<<("Su saldo ahora es :$") << balance <<("'\n'");
+         accbal1=balance;
+        // balance=0;
+        }// segundo if transferencia
+        else{cout << ("El dinero en su cuenta es insuficiente'\n'");}
+    }
+    else if (answ==2)
+    {
+    
+        if (balance>=dinero)
+        {
+         balance=balance-dinero;
+         cout<<("Su saldo ahora es :$") << balance <<("'\n'");
+         accbal2=balance;
+         //balance=0;
+        }// segundo if transferencia
+        else{cout << ("El dinero en su cuenta es insuficiente'\n'");}
+    }
+    else if (answ==3)
+    {
+        
+        if (balance>=dinero)
+        {
+         balance=balance-dinero;
+         cout<<("Su saldo ahora es") << balance <<("'\n'");
+         accbal3=balance;
+        // balance=0;
+        }// segundo if transferencia
+        else{cout << ("El dinero en su cuenta es insuficiente'\n'");}
+    }
+    else
+    {cout<< ("Porfavor ingrese datos correctos '\n'"); }//fin if transferencia
+        
+   
     break; 
+case 4:
+    cout<<("Para continuar ingrese su NIP actual: ");
+    cin>> answ;
+
+      if (answ==passuser1 and entrada==1) //entrada al usuario 1
+      {
+       cout << (" Ahora ingrese el nuevo NIP: ");
+       cin >> newpass;
+       cout<<("'\n'Confirme su nuevo NIP porfavor: ");
+       cin>>answ;
+       tamnips=to_string(newpass);
+        tamnip=tamnips.length();
+        if (newpass==answ and tamnip==4)
+        {
+        passuser1=newpass;
+        }else{cout<<("El nip no tiene la longitud correcta'\n'");}//condicion para confirmar el nip us1
+
+      }
+    else if (answ==passuser2 and entrada==2)//entrada al usuario 2
+    {
+       cout << ("'\n'Ahora ingrese el nuevo NIP: ");
+       cin >> newpass;
+       cout<<("'\n'Confirme su nuevo NIP porfavor: ");
+       cin>>answ;
+       tamnips=newpass;
+       tamnip=tamnips.length();
+        if (newpass==answ and tamnip==4)
+        {
+        passuser2=newpass;
+        cout<< ("Transacción realizada con éxito")<<endl;
+        }else{cout<<("El nip no tiene la longitud correcta'\n'");}
+    }    
+    else if (answ==passuser3 and entrada==3) //entrada al usuario 3
+    {
+       cout << ("'\n'Ahora ingrese el nuevo NIP: ");
+       cin >> newpass;
+       cout<<("'\n'Confirme su nuevo NIP porfavor: ");
+       cin>>answ;
+        tamnips=newpass;
+        tamnip=tamnips.length();
+        if (newpass==answ and tamnip==4)
+        {
+        passuser3=newpass;
+        cout<< ("Transacción realizada con éxito")<<endl;
+        }else{cout<<("El nip no tiene la longitud correcta'\n'");}       
+    }else{cout << ("'\n' El Nip no es correcto '\n'");}
+ 
+
+    
+    break;// break caso 4
 case 0:
     cout << ("Usted ha salido, Gracias por su preferencia'\n'");
     entrada=0;
@@ -126,7 +262,7 @@ default:
 
 }
 while (answ!=0) ; // while para el manejo de la cuenta
-} //if de manejo de cuenta
+  } //endif de manejo de cuenta
  }   //segundo while
 }//primer while "DO"
 while (entrada!=4); // instruccion do
